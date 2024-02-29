31e13c6a6916fbda2e478220eb8c1494bc636103
Nishan Poojary <npoojary@iu.edu>
2024-02-29 16:15:58 -0500

    Added Hosts

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%&&tc(@b+g09g5s65&xbfmrp+20z62+9qd*2l9hd98g*!^20)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'ciphers-service-latest-nishanpoojary.onrender.com']
 
 
 # Application definition
